dafed954021518e5930f1de771b7907ac1a8a3e1
NP288888888 <162107583+NP288888888@users.noreply.github.com>
2024-03-04 20:55:05 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Nguyễn Bảo Yến';
+const giftUrl = 'https://350new.com';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'My Frield';
+const contentCard = 'Chúc em 08/03 tràn ngập niềm vui và những nụ cười. Những điều tuyệt vời nhất sẽ dành cho em không chỉ trong ngày hôm nay mà còn trong những ngày tiếp theo.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Wecomel https://www.facebook.com/vn.NguyenAnhPhuong
+// Các bạn muốn học lập trình thì tham gia nhóm tự học lập trình nhé: https://www.facebook.com/vn.NguyenAnhPhuong
+const giftImage = '8888-.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
